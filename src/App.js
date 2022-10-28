c9302b8b3b8bf83fb90be6a2e7eb5fc09b6a3817
kaferas <chrisirak95@gmail.com>
2022-10-28 15:13:04 +0200

    Making Change Color App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
 
 function App() {
+const [colored, setColor] = useState("#0000b2")
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p style={{color:colored}}>
+          {
+             setInterval(()=>{
+              setColor("#"+
+                Math.floor(Math.random()*16777215).toString(16)
+              )
+            },3000)
+          }
+          This Kaferas Your Instructor Of Everyday
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
